733f5a9b59994f5eaa6df903ed749df1c1a52f89
buoyantair <buoyantair@protonmail.com>
2019-02-06 22:21:34 +0530

    basic homepage layout

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button>
+    <slot>
+    </slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+
+<style scoped lang="scss">
+button {
+  height: 40px;
+  width: 150px;
+  border-radius: 10px;
+  margin: 20px auto;
+  border: none;
+  background: #6197FF;
+  color: #fff;
+  font-weight: normal;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  font-size: 18px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="site-heading">
+      GitHub
+      <span>
+        Explorer
+      </span>
+    </h1>
+
+    <form>
+      <input type="text" placeholder="Search Github..."/>
+      <Button type="submit">Search</Button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Button from '@/components/Button.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+}
+.site-heading {
+  margin: 0 auto;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 72px;
+  font-weight: 800;
+  letter-spacing: -0.05em;
+  span {
+    font-size: 64px;
+    font-weight: 600;
+  }
+}
+
+form {
+  margin: 20px 0;
+  font-weight: normal;
+  input[type="text"] {
+    height: 30px;
+    width: 40%;
+    display: block;
+    margin: 0 auto;
+    border: 1px solid #C6C6C6;
+    border-radius: 10px;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    padding: 10px;
+    font-size: 18px;
+  }
+  input::placeholder {
+    color: #D8D8D8;
+  }
+}
+</style>
